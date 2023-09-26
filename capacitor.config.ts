d82cd667646df5fb27a711b9f5faabddad516c8d
Ehiosun Bishop <ehiosunbishop@gmail.com>
2023-09-26 03:12:49 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'callkeep-demo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
